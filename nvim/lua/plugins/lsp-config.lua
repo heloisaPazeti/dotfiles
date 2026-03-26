0e87a60bf22a7ae6f21c56dd1001e45982a161c9
NinjaPro923 <ninjapro93lu@gmail.com>
2026-03-26 07:18:10 -0300

    feat(nvim): lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,9 +1,87 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+    -- Mason: gerenciador de servidores
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    -- Ponte entre mason e lspconfig
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",  -- Lua
+                    "gopls",   -- Go
+                    "clangd",  -- C/C++
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+
+    -- nvim-lspconfig (só para habilitar os servers via nova API)
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+
+            -- Lua
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
+                        },
+                    },
+                },
+            })
+
+            -- Go
+            vim.lsp.config("gopls", {})
+
+            -- C/C++
+            vim.lsp.config("clangd", {})
+
+            -- Habilita os servidores
+            vim.lsp.enable({ "lua_ls", "gopls", "clangd", "pyright" })
+
+            -- Keymaps (só ativam quando LSP conecta no buffer)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                end,
+            })
+
+            -- Diagnósticos
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                float = {
+                    border = "rounded",
+                },
+            })
+        end,
+    },
 }
